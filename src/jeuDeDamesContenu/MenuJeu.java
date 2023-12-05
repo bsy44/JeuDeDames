433ed5528c8b3b661edd25707cea8d245256a960
bsy44 <syboubacar444@gmail.com>
2023-12-05 15:06:49 +0100

    comit 1 du menu Jeu

--- src/jeuDeDamesContenu/MenuJeu.java
+++ src/jeuDeDamesContenu/MenuJeu.java
@@ -1,4 +1,24 @@
 package jeuDeDamesContenu;
 
+import java.util.Scanner;
+
 public class MenuJeu {
+
+    public static void menuDebutJeu(){
+        Scanner scanner = new Scanner(System.in);
+
+        int menu;
+        System.out.println("1. afficher les règles");
+        System.out.println("2. démarrer une partie");
+        System.out.println("3. quitter");
+        menu = scanner.nextInt();
+
+        switch (menu){
+
+            case 1 :
+                System.out.println("2 joueurs s'affrontent sur un plateau de 10 cases en largeur et 10 cases en longuer (pion blanc VS pion noir)." +
+                                    "\nChaque Joueur possède 20 pions et peuvent les déplacer sur leur diagonale vers l'avant." +
+                                    "\n");
+        }
+    }
 }
