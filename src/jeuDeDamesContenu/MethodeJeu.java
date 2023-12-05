b85246c2c1a03bdaa92fa3ec35ca202dede188dc
Logan Dequier <logann.dequier@gmail.com>
2023-12-05 14:54:38 +0100

    Ajout couleur pions joueurs

--- src/jeuDeDamesContenu/MethodeJeu.java
+++ src/jeuDeDamesContenu/MethodeJeu.java
@@ -1,4 +1,13 @@
 package jeuDeDamesContenu;
 
 public class MethodeJeu {
+
+    public static int premierJoueur (){
+        int premierJ = 2,r;
+        r=(int) Math.random();
+
+        if (r<0.5)
+            premierJ=1;
+        return premierJ;
+    }
 }
